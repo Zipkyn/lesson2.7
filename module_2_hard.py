79b43ebd813cb6391199ffa003dabb6eba646a40
Zipkyn <v.t.s.782003@gmail.com>
2024-08-16 20:45:44 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,9 @@
+def generate_password(n):
+    password = ""
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if (i + j) % n == 0:
+                password += str(i) + str(j)
+    return password
+for n in range(3, 21):
+    print(f"{n} - {generate_password(n)}")
